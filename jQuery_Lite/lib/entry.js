e6867c3b382eb82213ae79c861529e8de733d39c
App Academy Student <student@appacademy.io>
2018-03-22 18:12:38 -0700

    End of the Day

--- jQuery_Lite/lib/entry.js
+++ jQuery_Lite/lib/entry.js
@@ -1,10 +1,21 @@
 const DOMNodeCollection = require ('./dom_node_collection');
 
 Window.prototype.$l = function (argument) {
+  const queue = [];
+
+  if (argument instanceof Function) {
+    queue.push(argument);
+  }
+  document.addEventListener("DOMContentLoaded", runQueue);
+
+  function runQueue () {
+    queue.forEach((el) => el());
+  }
+
   if (argument instanceof HTMLElement) {
     return new DOMNodeCollection(Array.from(argument));
   }
-  
+
   let nodelist = document.querySelectorAll(argument);
   return new DOMNodeCollection(Array.from(nodelist));
 };
--- jQuery_Lite/lib/jquery_lite.js
+++ jQuery_Lite/lib/jquery_lite.js
@@ -80,63 +80,115 @@
 
 class DOMNodeCollection {
   constructor (HTMLArray) {
-    this.array = HTMLArray;
+    this.elements = HTMLArray;
   }
 
   html (string) {
-    if (string) {
-      this.innerHTML = string;
-      return this;
-    } else {
-      return this.innerHTML;
+    for (let i = 0; i < this.elements.length; i++) {
+      if (string) {
+        this.elements[i].innerHTML = string;
+      } else {
+        return this.elements[0].innerHTML;
+      }
     }
-
   }
 
   empty () {
-    this.html = "";
+    for (let i = 0; i < this.elements.length; i++) {
+      this.elements[i].innerHTML = "";
+    }
   }
 
-  append (arg) {
-    if (arg instanceof HTMLElement) {
-      arg.outerHTML = this.innerHTML;
-    } else if (arg instanceof String) {
-      this.innerHTML = arg;
-    } else if (arg instanceof DOMNodeCollection) {
-      arg.outerHTML = this.innerHTML;
-    } else {
-      return undefined;
+  append (...args) {
+    for (var i = 0; i < this.elements.length; i++) {
+      for (var j = 0; j < args.length; j++) {
+        // debugger
+        if (args[j] instanceof HTMLElement) {
+          this.elements[i].appendChild(args[j]);
+        } else if (typeof args[j] === "string") {
+          let argObj = document.createElement(args[j]);
+          this.elements[i].appendChild(argObj);
+        } else if (args[j] instanceof DOMNodeCollection) {
+          args[j].outerHTML = this.elements[i].innerHTML;
+          this.elements[i].innerHTML = args[j].outerHTML;
+        } else {
+          return undefined;
+        }
+      }
     }
   }
 
   attr (name, value) {
-    this.setAttribute(name, value);
+    for (let i = 0; i < this.elements.length; i++) {
+      this.elements[i].setAttribute(name, value);
+    }
   }
 
-  addClass () {
-
+  addClass (adClass) {
+    for (let i = 0; i < this.elements.length; i++) {
+      this.elements[i].classList.add(adClass);
+    }
   }
 
-  removeClass () {
-
+  removeClass (remClass) {
+    for (let i = 0; i < this.elements.length; i++) {
+      this.elements[i].classList.remove(remClass);
+    }
   }
 // traversal
-
+// returns DNC of all children in an array
   children () {
+    let children = [];
+    for (let i = 0; i < this.elements.length; i++) {
+      let nodelist = this.elements[i].childNodes;
+      children = children.concat(Array.from(nodelist));
+    }
 
+    return new DOMNodeCollection(children);
   }
 
   parent () {
+    let parents = [];
+    for (let i = 0; i < this.elements.length; i++) {
+      let parent = this.elements[i].parentNode;
 
+      parents.push(parent);
+    }
+
+    return new DOMNodeCollection(parents);
   }
 
-  find () {
 
+
+  find (attr) {
+    let matched = [];
+
+    for (let i = 0; i < this.elements.length; i++) {
+      let found = Array.from(this.elements[i].querySelectorAll(attr));
+      matched = matched.concat(found);
+    }
+    return new DOMNodeCollection(matched);
   }
 
   remove () {
-
+    this.elements.forEach((el) => el.remove());
   }
+
+  on (eventType, callback) {
+    for (let i = 0; i < this.elements.length; i++) {
+      this.elements[i].addEventListener(eventType, callback);
+      this.elements[i].setAttribute('eventScript', callback);
+    }
+  }
+
+  off (eventType) {
+    for (let i = 0; i < this.elements.length; i++) {
+      let callback = this.elements[i].getAttribute('eventScript');
+      this.elements[i].removeAttribute('eventScript');
+      this.elements[i].removeEventListener(eventType, callback);
+    }
+  }
+
 
 }
 
@@ -155,10 +207,21 @@ module.exports = DOMNodeCollection;
 const DOMNodeCollection = __webpack_require__ (/*! ./dom_node_collection */ "./lib/dom_node_collection.js");
 
 Window.prototype.$l = function (argument) {
+  const queue = [];
+
+  if (argument instanceof Function) {
+    queue.push(argument);
+  }
+  document.addEventListener("DOMContentLoaded", runQueue);
+
+  function runQueue () {
+    queue.forEach((el) => el());
+  }
+
   if (argument instanceof HTMLElement) {
     return new DOMNodeCollection(Array.from(argument));
   }
-  
+
   let nodelist = document.querySelectorAll(argument);
   return new DOMNodeCollection(Array.from(nodelist));
 };
